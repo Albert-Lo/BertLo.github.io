ebe62ea3b20e458babae36c9a4d77b8e1c25d0df
Albert <sero.albert@gmail.com>
2016-05-23 22:02:29 -0700

    Add depth perspective for parallax effect

--- app/elm-modules/Home.elm
+++ app/elm-modules/Home.elm
@@ -11,7 +11,7 @@ import List
 import Window
 import Mouse exposing (Position)
 import Task exposing (perform)
-import Random exposing (float, int, map3)
+import Random exposing (float, int, map4)
 
 main =
   Html.program {init = init, view = view, update = update, subscriptions = subscriptions}
@@ -19,17 +19,12 @@ main =
 
 -- MODEL
 
-type alias MeteorInit = {
-  x: Int
-  , y: Int
-  , end: Int
-}
-
 type alias Meteor = {
   x: Int
   , y: Int
   , current: Int
   , end: Int
+  , depth: Int
 }
 
 type alias Model = {
@@ -55,7 +50,7 @@ init =
 type Msg
   = Wheel
   | Prob Float
-  | Add MeteorInit
+  | Add Meteor
   | Resize Window.Size
   | Move Position
 
@@ -79,10 +74,11 @@ progressMeteors model =
 
 minLength = 240
 maxLength = 450
+maxDepth = 4
 
 newMeteor: Int -> Int -> Cmd Msg
 newMeteor windowWidth windowHeight=
-    map3 (\x y end -> MeteorInit x y end) (int maxLength windowWidth) (int 1 (windowHeight - maxLength)) (int minLength maxLength)
+    map4 (\x y end depth -> Meteor x y 0 end depth) (int maxLength windowWidth) (int 0 (windowHeight - maxLength)) (int minLength maxLength) (int 1 maxDepth)
       |> Random.generate Add
 
 update : Msg -> Model -> (Model, Cmd Msg)
@@ -98,12 +94,12 @@ update msg model =
           (model, newMeteor model.windowWidth model.windowHeight)
         else
           (model, Cmd.none)
-      Add mi ->
+      Add new ->
         let
-          shouldAdd = List.all (\m -> abs ((lineOffset m model.windowHeight) - (lineOffset mi model.windowHeight)) > 70)  model.meteors
+          shouldAdd = List.all (\m -> abs ((lineOffset m model.windowHeight) - (lineOffset new model.windowHeight)) > 50)  model.meteors
         in
           if shouldAdd then
-            ({model | meteors = model.meteors ++ [(Meteor mi.x mi.y 0 mi.end)]}, Cmd.none)
+            ({model | meteors = model.meteors ++ [new]}, Cmd.none)
           else
             (model, Cmd.none)
       Resize size ->
@@ -136,57 +132,58 @@ stopPoint current end =
   else
     toString tailLength
 
-perspectiveOffset : Int -> Int -> Int -> Int -> String
-perspectiveOffset mouseX mouseY windowWidth windowHeight =
+perspectiveOffset : Int -> Int -> Float
+perspectiveOffset mouse window =
   let
-    maxOffset = 40
+    maxOffset = 100
   in
-    "top:"
-    ++ (toString (((((toFloat windowHeight) / 2.0) - (toFloat mouseY)) / (toFloat windowHeight)) * maxOffset))
-    ++ "px;"
-    ++ "left:"
-    ++ (toString (((((toFloat windowWidth) / 2.0) - (toFloat mouseX)) / (toFloat windowWidth)) * maxOffset))
-    ++ "px;"
+    ((((toFloat window) / 2.0) - (toFloat mouse)) / (toFloat window)) * maxOffset
 
-
+depthOffset : Int -> Float -> Int -> Int
+depthOffset org offset depth =
+  org + (round (offset * (1.0 / (toFloat depth))))
 
 view : Model -> Html.Html Msg
 view model =
-  div [class "Elm-Home", onWheel Wheel] [
-    svg [width "100%", height "100%", style ("position: absolute;" ++ (perspectiveOffset model.mouseX model.mouseY model.windowWidth model.windowHeight))] (
-      (List.indexedMap (\i m -> linearGradient [
-        id ("gradient" ++ (toString i)), x1 "0%", y1 "0%", x2 "100%", y2 "0%"
-      ] [
-        stop [offset "0%", style "stop-color: rgb(255, 255, 255); stop-opacity: 1"] []
-        , stop [offset ((stopPoint m.current m.end) ++ "%"), style "stop-color: rgb(255, 255, 255); stop-opacity:0"] []
-      ]) model.meteors)
-      ++ (List.indexedMap (\i m -> (line [
-        x1 (toString m.x)
-        , y1 (toString m.y)
-        , x2 (toString (m.x - (min m.current m.end)))
-        , y2 (toString (m.y + (min m.current m.end)))
-        , stroke ("url(#gradient" ++ (toString i) ++ ")")
-      ] [])) model.meteors)
-    )
-    , div [class "text"] [
-      div [class "name"] [text "Albert Lo"]
-      , div [class "primary"] [
-        a [href "https://xkcd.com/844/", target "_blank"] [text "software engineer"]
-        , text " / "
-        , a [href "https://xkcd.com/1678/", target "_blank"] [text "generalist"]
-      ]
-      , div [class "secondary"] [
-        a [href "https://xkcd.com/1537/", target "_blank"] [text "js ethuiast"]
-        , text " / "
-        , a [href "https://xkcd.com/1270/", target "_blank"] [text "lambda fanatic"]
-      ]
-      , hr [] []
-      , div [class "links"] [
-        a [href "https://github.com/BertLo", target "_blank"] [text "github"]
-        , text " / "
-        , a [href "https://www.linkedin.com/in/alberthtlo", target "_blank"] [text "linkedin"]
-        , text " / "
-        , a [href "/resume", target "_blank"] [text "resume"]
+  let
+    xOffset = perspectiveOffset model.mouseX model.windowWidth
+    yOffset = perspectiveOffset model.mouseY model.windowHeight
+  in
+    div [class "Elm-Home", onWheel Wheel] [
+      svg [width "100%", height "100%"] (
+        (List.indexedMap (\i m -> (linearGradient [
+          id ("gradient" ++ (toString i)), x1 "0%", y1 "0%", x2 "100%", y2 "0%"
+        ] [
+          stop [offset "0%", style "stop-color: rgb(255, 255, 255); stop-opacity: 1"] []
+          , stop [offset ((stopPoint m.current m.end) ++ "%"), style "stop-color: rgb(255, 255, 255); stop-opacity:0"] []
+        ])) model.meteors)
+        ++ (List.indexedMap (\i m -> (line [
+          x1 (toString (depthOffset m.x xOffset m.depth))
+          , y1 (toString (depthOffset m.y yOffset m.depth))
+          , x2 (toString (depthOffset (m.x - (min m.current m.end)) xOffset m.depth))
+          , y2 (toString (depthOffset (m.y + (min m.current m.end)) yOffset m.depth))
+          , stroke ("url(#gradient" ++ (toString i) ++ ")")
+        ] [])) model.meteors)
+      )
+      , div [class "text"] [
+        div [class "name"] [text "Albert Lo"]
+        , div [class "primary"] [
+          a [href "https://xkcd.com/844/", target "_blank"] [text "software engineer"]
+          , text " / "
+          , a [href "https://xkcd.com/1678/", target "_blank"] [text "generalist"]
+        ]
+        , div [class "secondary"] [
+          a [href "https://xkcd.com/1537/", target "_blank"] [text "js ethuiast"]
+          , text " / "
+          , a [href "https://xkcd.com/1270/", target "_blank"] [text "lambda fanatic"]
+        ]
+        , hr [] []
+        , div [class "links"] [
+          a [href "https://github.com/BertLo", target "_blank"] [text "github"]
+          , text " / "
+          , a [href "https://www.linkedin.com/in/alberthtlo", target "_blank"] [text "linkedin"]
+          , text " / "
+          , a [href "/resume", target "_blank"] [text "resume"]
+        ]
       ]
     ]
-  ]
